96c5ee384bc393f7e17b96a54e409e4afebe31c4
Jeppe Klitgaard <hi@jeppe.science>
2021-10-21 18:30:38 +0100

    Feat: Improve error handling slightly.
    Sadly no way to get rid of net:ERR_CONNECTION_REFUSED
    as that comes straight from Chromium

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { ISettings, DEFAULT_SETTINGS } from 'settings/settings'
 import { SettingsTab } from 'settings/settings-tab'
 import { CardsService } from 'services/cards'
 import { Anki } from 'services/anki'
-import { noticeTimeout, flashcardsIcon, pluginName } from 'consts'
+import { flashcardsIcon } from 'consts'
 import { Reader } from 'services/reader'
 import { Bridge } from 'services/bridge'
 
@@ -16,6 +16,7 @@ export default class AnkiBridgePlugin extends Plugin {
     private bridge: Bridge
 
     private statusbar: HTMLElement
+    public connectionStatus = false
 
     async onload() {
         addIcon('flashcards', flashcardsIcon)
@@ -34,13 +35,7 @@ export default class AnkiBridgePlugin extends Plugin {
             id: 'anki-bridge-sync-open-file',
             name: 'Sync file with Anki',
             callback: async () => {
-                const activeFile = this.app.workspace.getActiveFile()
-                if (activeFile) {
-                    await this.syncFile(activeFile)
-                    new Notice("Synced with Anki ✔")
-                } else {
-                    new Notice("Please open a file first")
-                }
+                await this.syncActiveFile()
             },
         })
 
@@ -48,37 +43,17 @@ export default class AnkiBridgePlugin extends Plugin {
             id: 'anki-bridge-sync-all-files',
             name: 'Sync all files with Anki',
             callback: async () => {
-                new Notice("Syncing all files with Anki...")
-                await Promise.all(this.app.vault.getMarkdownFiles().map(async (file) => {
-                    await this.syncFile(file)
-                }))
-
-                new Notice ("Synced all files with Anki ✔")
+                await this.syncAllFiles()
             },
         })
 
         this.addRibbonIcon('flashcards', 'Sync with Anki', async () => {
-            const activeFile = this.app.workspace.getActiveFile()
-            if (activeFile) {
-                await this.syncFile(activeFile)
-                new Notice("Synced with Anki ✔")
-            } else {
-                new Notice('Please open a file first')
-            }
+            await this.syncActiveFile()
         })
 
         this.addSettingTab(new SettingsTab(this.app, this))
 
-        this.registerInterval(
-            window.setInterval(
-                () =>
-                    this.anki
-                        .ping()
-                        .then(() => this.statusbar.setText('Anki ✔'))
-                        .catch(() => this.statusbar.setText('Anki ❌')),
-                this.settings.pollInterval,
-            ),
-        )
+        await this.pingAnki()
     }
 
     async onunload() {
@@ -103,8 +78,104 @@ export default class AnkiBridgePlugin extends Plugin {
         console.error(this.manifest.name + ': ' + text)
     }
 
-    private async syncFile(file: TFile): Promise<void> {
+    public async pingAnki(): Promise<boolean> {
+        let hasErrored: boolean
+        try {
+            await this.anki.ping()
+            hasErrored = false
+        } catch (e) {
+            hasErrored = true
+        }
+
+        this.setConnectionStatus(!hasErrored)
+        return !hasErrored
+    }
+
+    private setConnectionStatus(status: boolean): void {
+        this.connectionStatus = status
+
+        const statusbarText = 'Anki ' + (status ? '✔' : '❌')
+        this.statusbar.setText(statusbarText)
+    }
+
+    public getConnectionStatus(): boolean {
+        return this.connectionStatus
+    }
+
+    private printFailedConnection(): void {
+        const errMsg = 'Failed to connect to Anki ❌\n\nIs it running?'
+        new Notice(errMsg)
+    }
+
+    /**
+     * Can raise an error
+     */
+    private async syncFileRoutine(file: TFile): Promise<void> {
         const elements = await this.reader.readFile(file)
         await this.bridge.processFileResults(elements)
+    }
+
+    private handleSyncFileError(error: string): void {
+        if (error == 'failed to issue request') {
+            this.setConnectionStatus(false)
+            this.printFailedConnection()
+        } else {
+            throw error
+        }
+    }
+
+    private async syncFile(file: TFile): Promise<boolean> {
+        let hasErrored: boolean
+        try {
+            await this.syncFileRoutine(file)
+            hasErrored = false
+        } catch (e) {
+            hasErrored = true
+            this.handleSyncFileError(e)
+        }
+
+        return !hasErrored
+    }
+
+    private async syncActiveFile(): Promise<void> {
+        const activeFile = this.app.workspace.getActiveFile()
+        if (activeFile) {
+            if (!(await this.pingAnki())) {
+                this.printFailedConnection()
+                return
+            }
+            if (await this.syncFile(activeFile)) {
+                new Notice('Synced with Anki ✔')
+            }
+        } else {
+            new Notice('Please open a file first')
+        }
+    }
+
+    private async syncAllFiles(): Promise<void> {
+        new Notice('Syncing all files with Anki...')
+
+        if (!(await this.pingAnki())) {
+            this.printFailedConnection()
+            return
+        }
+
+        let hasErrored: boolean
+        try {
+            await Promise.all(
+                this.app.vault.getMarkdownFiles().map(async (file) => {
+                    await this.syncFileRoutine(file)
+                }),
+            )
+
+            hasErrored = false
+        } catch (e) {
+            hasErrored = true
+            this.handleSyncFileError(e)
+        } finally {
+            if (!hasErrored) {
+                new Notice('Synced all files with Anki ✔')
+            }
+        }
     }
 }
