8cd502550285a350f7a08cafb96ebafbaecc95d6
Jeppe Klitgaard <hi@jeppe.science>
2021-10-21 22:45:56 +0100

    chore: clean up package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import copy from 'rollup-plugin-copy'
 import json from '@rollup/plugin-json';
 
 const isProd = (process.env.BUILD === 'production');
