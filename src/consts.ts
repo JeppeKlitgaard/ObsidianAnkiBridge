ae9a511b88a1e082a25c43b776d6c3b2a5cbf426
Jeppe Klitgaard <hi@jeppe.science>
2022-01-19 23:12:18 +0000

    feat: migration warning
    Fix: #5

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import libGrammar from 'grammars/Lib.pegjs'
 
 // 1: Initial version
-export const LATEST_MIGRATION_VERSION = 1
+export const LATEST_MIGRATION_VERSION = 0
 
 export const PLUGIN_NAME = 'AnkiBridge'
 export const DOCUMENTATION_URL = 'https://jeppeklitgaard.github.io/ObsidianAnkiBridge/'
