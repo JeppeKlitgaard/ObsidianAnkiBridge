027b72f2497004d726dabf3b384549ef4eed953a
tnewing <newing.timothy@outlook.com>
2023-08-11 14:15:58 +1000

    Added support for wikilink heading embeds

--- src/processors/preprocessors/embed.ts
+++ src/processors/preprocessors/embed.ts
@@ -2,7 +2,8 @@ import { NoteBase } from 'ankibridge/notes/base'
 import { ProcessorContext } from 'ankibridge/processors/base'
 import { Preprocessor } from 'ankibridge/processors/preprocessors/base'
 import { stripCr } from 'ankibridge/utils/file'
-import { TFile, parseLinktext, resolveSubpath,  } from 'obsidian'
+import { link } from 'fs'
+import { TFile, parseLinktext, resolveSubpath, Notice, stripHeading } from 'obsidian'
 
 export class EmbedPreprocessor extends Preprocessor {
     /**
@@ -20,18 +21,53 @@ export class EmbedPreprocessor extends Preprocessor {
     ): Promise<string | null> {
         let file: TFile | null = null;
         if (strField !== null) {
-            const embedRegex = /!\[(.*?)]\((.*?)\)/gis;
-            for (const match of strField.matchAll(embedRegex)) {
+            const embedRegex_md = /!\[(.*?)]\((.*?)\)/gis;
+            for (const match of strField.matchAll(embedRegex_md)) {
                 const result = parseLinktext(match[2]);
                 const subpath = result.subpath.replace(/%20/gis, " ");
-                if (result.path == "") {
+                const path = result.path.replace(/%20/gis, " ");
+                if (path == "") {
                     // Check if path is a Tfile
                     if (note.source.file instanceof TFile) {
                         file = note.source.file;
                     }
                 } else {
                     // Check if path is a Tfile
-                    const abstractFile = this.app.vault.getAbstractFileByPath(result.path);
+                    const abstractFile = this.app.vault.getAbstractFileByPath(path);
+                    if (abstractFile instanceof TFile) {
+                        file = abstractFile;
+                    }
+                }
+                if (file !== null) {
+                    const cache = this.app.metadataCache.getFileCache(file);
+                    if (cache !== null) {
+                        const loc = resolveSubpath(cache, subpath);
+                        if (loc.end !== null) {
+                            const filetext = stripCr(await this.app.vault.read(file));
+                            const splitLines = filetext.split("\n");
+                            const filetextlines = splitLines.slice(loc.start.line, loc.end.line).join("\n");
+                            strField = strField.replace(match[0], filetextlines);
+                        }
+                    }
+                }
+            }
+
+            const embedRegex_wiki = /!\[\[(.*?)\]\]/gis;
+            for (const match of strField.matchAll(embedRegex_wiki)) {
+                const linkstring = match[1] as string;
+                // Extract subpath from display text, just in case 
+                const linktext = linkstring.split("|")[0];
+                const result = parseLinktext(linktext);
+                const subpath = result.subpath;
+                const path = result.path;
+                if (path == "") {
+                    // Check if path is a Tfile
+                    if (note.source.file instanceof TFile) {
+                        file = note.source.file;
+                    }
+                } else {
+                    // Check if path is a Tfile
+                    const abstractFile = this.app.vault.getAbstractFileByPath(path);
                     if (abstractFile instanceof TFile) {
                         file = abstractFile;
                     }
